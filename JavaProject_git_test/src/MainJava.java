d75fdcc8e03bbc2ed3f98c218167620153cc5723
myGit <myGit@>
2020-07-24 21:04:12 +0800

    第三次克隆提交

--- JavaProject_git_test/src/MainJava.java
+++ JavaProject_git_test/src/MainJava.java
@@ -2,5 +2,6 @@ public class MainJava {
     public static void main(String[] args) {
         System.out.println("git和idea11");
         System.out.println("git第一次修改");
+        System.out.println("克隆修改");
     }
 }
